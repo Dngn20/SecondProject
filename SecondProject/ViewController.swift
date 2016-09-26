ecd2f9f9eb529136a52f37983cd46d8dbe5e1a89
Dngn20 <dngu9879@740s-pl-998.local>
2016-09-26 13:55:09 -0600

    Added Color Change

--- SecondProject/ViewController.swift
+++ SecondProject/ViewController.swift
@@ -10,31 +10,54 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBOutlet var projectActiivity:
-        UIView!
-    @IBOutlet weak var projectSwitch:
-        UISwitch!
-    @IBAction func projectSwticher(sender: UISwitch) {
+    @IBOutlet weak var projectButton:UIButton!
+    @IBOutlet var projectActiivity:UIView!
+    @IBOutlet weak var projectSwitch:UISwitch!
+    @IBAction func projectSwticher(sender: UISwitch)
+    {
+    
     }
-    @IBOutlet weak var projectSlider:
-        UISlider!
-    @IBOutlet weak var textLabel:
-        UIButton!
-
+    @IBOutlet weak var projectSlider:UISlider!
+    @IBOutlet weak var textLabel:UIButton!
+    @IBOutlet var appBackground: UIView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func projectSwitchAction(sender: UISwitch) -> Void
+    {
+        
+    }
+    @IBAction func ColorButton(sender: UIButton)
+    {
+    appBackground.backgroundColor = makeRandomColor()
     
+        
     }
-
-    @IBAction func projectSwitchAction(sender: UISwitch) {
+    
+    private func makeRandomColor() -> UIColor
+       {
+        let randomColor : UIColor
+        //Color in Swift is a percentage -  from 0-1 inclusive
+        //Of type Float for all componentss: RGB
+           let redColor :CGFloat = CGFloat(drand48())
+           let greenColor :CGFloat =  CGFloat(Double(arc4random_uniform(256))/255.00)
+           let blueColor : CGFloat = CGFloat(drand48())
+           let alphaChannel :CGFloat = 1.0
+        
+      randomColor = UIColor(red : redColor, green: greenColor, blue: blueColor, alpha: alphaChannel)
+        
+        return randomColor
+        
     }
-
 }
 
